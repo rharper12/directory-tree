e7a8905816b637c2945170df6f659fcd28c80df1
rharper24 <ronharper938@gmail.com>
2024-12-20 14:05:11 -0600

    chore(directory.ts): cleaned up unused variables

--- src/types/directory.ts
+++ src/types/directory.ts
@@ -8,31 +8,6 @@ export interface Result<T = void> {
   error?: string;
   data?: T;
 }
-
-export type DirectoryCommand = 'CREATE' | 'MOVE' | 'DELETE' | 'LIST';
-
-export interface BaseCommandBody {
-  command: DirectoryCommand;
-  path?: string;
-}
-
-export interface MoveCommandBody extends BaseCommandBody {
-  command: 'MOVE';
-  path: string;
-  destPath: string;
-}
-
-export interface StandardCommandBody extends BaseCommandBody {
-  command: Exclude<DirectoryCommand, 'MOVE'>;
-  path: string;
-}
-
-export interface ListCommandBody extends BaseCommandBody {
-  command: 'LIST';
-}
-
-export type CommandBody = MoveCommandBody | StandardCommandBody | ListCommandBody;
-
 export interface DirectoryResponse {
   message?: string;
   error?: string;
